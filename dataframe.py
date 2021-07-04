7fe76e89c1e2c7f2f655e7448da3f7984614a1cd
foysal <foysalf652@gmail.com>
2021-07-04 06:40:53 +0600

    matplotlib

--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,9 @@
 #We can use an array or a database table to structure or present data.#
 
 #Example of an array:#
+from numpy import inf
 import pandas as pd
+from pandas.core import indexing
 
 d = {'col1': [1, 2, 3, 4, 7], 'col2': [4, 5, 6, 9, 5], 'col3': [7, 8, 12, 1, 11]}
 
@@ -13,6 +15,7 @@ print(df)
 
 df = pd.DataFrame(data=d)
 count_column = df.shape[1]
+print("")
 print("number of column:")
 print(count_column)
 
@@ -21,3 +24,9 @@ print(count_column)
 # Array = [80, 85, 90, 95, 100, 105, 110, 115, 120, 125]
 # print(Array)
 # print(Array+ [150, 160])
+
+#use the read_csv() function to import a CSV file with the health data:
+health_data = pd.read_csv("data.csv", header=0, sep=",")
+
+print(health_data)
+
